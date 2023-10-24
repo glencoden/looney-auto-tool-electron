dfdbce4d9f570bcc93943f019edbaa1fe9fb2a55
glen coden <info@simonmeyer.de>
2023-10-24 14:02:32 +0200

    add styles

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        titleBarStyle: 'hidden',
+        transparent: true,
     })
 
     win.loadFile('index.html')
